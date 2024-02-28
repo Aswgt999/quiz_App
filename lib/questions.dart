119e0186d33707f74a6d5453f819aee34641ab49
Aswgt999 <108606894+Aswgt999@users.noreply.github.com>
2024-02-28 15:19:22 +0530

    updated message

--- lib/questions.dart
+++ lib/questions.dart
@@ -25,7 +25,7 @@ class _QuestionState extends State<Question> {
               alignment: Alignment.center,
               decoration: const BoxDecoration(color: Colors.black),
               child: const Text(
-                'hello',
+                'this is me',
                 style: TextStyle(color: Colors.white),
               ),
             ),
